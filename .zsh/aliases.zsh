668ade45c2a08eab8da84acfd1b5dbc92ae80302
Graham Lillico <63392565+glillico@users.noreply.github.com>
2024-05-30 23:44:10 +0100

    Modified aliases.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,13 +2,14 @@
 # General aliases
 ##################
 #alias subl="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
+alias ..='cd ..'
 alias code='open -a VSCodium'
-alias grep="grep --colour --devices=skip"
-alias history='history -E'
-alias hist='history -E 1'
-alias hub='cd ~/Development/GitHub'
 alias dev='cd ~/Development'
+alias grep='grep --colour --devices=skip'
+alias hist='history -E 1'
+alias history='history -E'
 alias homelab='cd ~/Development/homelab'
+alias hub='cd ~/Development/GitHub'
 if [ -f /opt/homebrew/bin/eza ]; then
   alias ls='eza --icons --group-directories-first --git --header'
   alias ll='eza --icons --group-directories-first --git --header -l'
@@ -17,68 +18,75 @@ fi
 ################
 # Play it safe!
 ################
-alias 'rm=rm -i'
-alias 'mv=mv -i'
 alias 'cp=cp -i'
+alias 'mv=mv -i'
+alias 'rm=rm -i'
 
 ###############
 # Git aliases.
 ###############
+# alias gbd='git branch -d'
+# alias gbD='git branch -D'
+# alias gc='git commit'
+# alias gcam='git commit -a -m'
+# alias gcb='git checkout -b'
+# alias gcm='git commit -m'
+# alias ggl='git pull origin'
+# alias ggp='git push origin'
+# alias ggu='git pull --rebase origin'
+# alias glog='git log --oneline --decorate --graph'
+# alias gloga='git log --oneline --decorate --graph --all'
+# alias gs='git status'
+# alias gsa='git status -uall'
+# alias gsb='git status --short --branch'
+# alias gss='git status --short'
 alias ga='git add'
 alias gaa='git add --all'
-
-alias gs='git status'
-alias gsa='git status -uall'
-alias gsb='git status --short --branch'
-alias gss='git status --short'
-
-alias gc='git commit'
-alias gcm='git commit -m'
-alias gcam='git commit -a -m'
-
-alias ggl='git pull origin'
-alias ggu='git pull --rebase origin'
-
-alias ggp='git push origin'
-
-alias gbd='git branch -d'
-alias gbD='git branch -D'
-
+alias gamend='git commit -a --amend --no-edit'
+alias gbr='git branch'
+alias gbra='git branch -a'
+alias gbrd='git branch -d'
+alias gbrD='git branch -D'
+alias gcdf='git clean -df'
+alias gci='git commit'
+alias gciam='git commit -a -m'
+alias gcim='git commit -m'
 alias gco='git checkout'
-alias gcb='git checkout -b'
-alias gcom="git checkout master"
-
-alias gf='git fetch'
-alias gfd='git fetch --dry-run'
-alias gfp='git fetch -pv'
-
-alias gm='git merge'
-
-alias gl='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit -n 10'
-alias gla='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
-alias gld='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset" --abbrev-commit -n 10'
-alias glda='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset" --abbrev-commit'
-alias glog='git log --oneline --decorate --graph'
-alias gloga='git log --oneline --decorate --graph --all'
-
-alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--WIP--"'
+alias gcob='git checkout -b'
+alias gcod='git checkout .'
+alias gcom='git checkout master'
+alias gconf='git config --global --edit'
+alias gfe='git fetch'
+alias gfed='git fetch --dry-run'
+alias gfep='git fetch -pv'
+alias glog='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit -n 10'
+alias gloga='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+alias glogd='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset" --abbrev-commit -n 10'
+alias glogda='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset" --abbrev-commit'
+alias gme='git merge'
+alias gpr='git pull --rebase'
+alias gpro='git pull --rebase origin'
+alias grh='git reset HEAD'
+alias gst='git status -s'
+alias gsta='git status'
 alias gunwip='git log -n 1 | grep -q -c "\-\-WIP\-\-" && git reset HEAD~1'
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--WIP--"'
 
 ###################
 # Vagrant aliases.
 ###################
-alias vgi="vagrant init"
-alias vup="vagrant up"
-alias vd="vagrant destroy"
-alias vdf="vagrant destroy -f"
-alias vssh="vagrant ssh"
-alias vh="vagrant halt"
-alias vst="vagrant status"
-alias vba="vagrant box add"
-alias vbr="vagrant box remove"
-alias vbl="vagrant box list"
-alias vbo="vagrant box outdated"
-alias vbu="vagrant box update"
+alias vba='vagrant box add'
+alias vbl='vagrant box list'
+alias vbo='vagrant box outdated'
+alias vbr='vagrant box remove'
+alias vbu='vagrant box update'
+alias vd='vagrant destroy'
+alias vdf='vagrant destroy -f'
+alias vgi='vagrant init'
+alias vh='vagrant halt'
+alias vssh='vagrant ssh'
+alias vst='vagrant status'
+alias vup='vagrant up'
 
 ##########################
 # mac-dev-playbook alias.
