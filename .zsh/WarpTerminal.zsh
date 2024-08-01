3b44a69d1a4ef28366b227c1e7e8447629dc7cf5
Graham Lillico <63392565+glillico@users.noreply.github.com>
2024-08-01 22:33:01 +0100

    Modified warp terminal prompt.

--- .zsh/WarpTerminal.zsh
+++ .zsh/WarpTerminal.zsh
@@ -1,5 +1,5 @@
 if [ -v SSH_CLIENT ] || [ -v SSH_TTY ]; then
-  MYPROMPT=$'\n%F{yellow}[%(?.%F{green}${PROMPT_DISTRO}.%F{red}?%?) %F{cyan}%n@%m %F{green}%*%f %F{blue}%3~%F{yellow}]%f%b'
+  MYPROMPT=$'\n%F{yellow}[%(?.%F{green}${PROMPT_DISTRO}.%F{red}?%?) %F{cyan}%n@%m %F{green}%*%f %F{blue}%3~%F{yellow}]%f%b'$'\n%# '
 else
-  MYPROMPT=$'\n%F{yellow}[%(?.%F{green}${PROMPT_DISTRO}.%F{red}?%?) %F{green}%*%f %F{blue}%3~%F{yellow}]%f%b'
+  MYPROMPT=$'\n%F{yellow}[%(?.%F{green}${PROMPT_DISTRO}.%F{red}?%?) %F{green}%*%f %F{blue}%3~%F{yellow}]%f%b'$'\n%# '
 fi
